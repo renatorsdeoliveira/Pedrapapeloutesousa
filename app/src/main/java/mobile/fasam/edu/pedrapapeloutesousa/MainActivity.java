20398183d7c6875d9fe79cb51783a5d44d2d142a
Renato_O <renatorsdeoliveira@gmail.com>
2019-10-19 15:58:45 -0300

    Inserido imagens e criados metodos

--- app/src/main/java/mobile/fasam/edu/pedrapapeloutesousa/MainActivity.java
+++ app/src/main/java/mobile/fasam/edu/pedrapapeloutesousa/MainActivity.java
@@ -3,6 +3,7 @@ package mobile.fasam.edu.pedrapapeloutesousa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selecionadoPedra(View view) {
+        
+    }
+
+    public void selecionadoPapel(View view) {
+
+    }
+
+    public void selecionadoTesoura(View view) {
+
     }
 }
